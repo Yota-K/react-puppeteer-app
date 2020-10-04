de2622176db31f8f247edb1ac94a11466a135623
紅梅陽太 <powdersugar828828@gmail.com>
2020-10-05 01:48:23 +0900

    コメントアウトの不備を修正。

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     filename: 'bundle.js',
   },
   watch: true,
-  // MEMO: DockerでLive Reloadを有効にするためには
+  // MEMO: DockerでLive Reloadを有効にするためにはオプションでポーリングを行うよう設定する必要がある
   // https://webpack.js.org/configuration/watch/
   // https://qiita.com/ayatas/items/e88064cebe6b3d893b0e
   watchOptions: {
