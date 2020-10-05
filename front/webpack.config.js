80ec4d68e1a01512668f72880618d9ae484f6976
紅梅陽太 <powdersugar828828@gmail.com>
2020-10-06 00:08:19 +0900

    CSSを扱うためのローダーを追加。

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -19,10 +19,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      },
+      }
     ]
   },
   plugins: [
