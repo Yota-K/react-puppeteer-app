822afb205aa2251a0c4b0f42a0ef3e5537343cec
紅梅陽太 <powdersugar828828@gmail.com>
2020-10-15 01:59:03 +0900

    Main.tsxにあったContainerをHome.tsxに移動。

--- front/src/components/Main.tsx
+++ front/src/components/Main.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import Result from '../pages/Result';
+import About from '../pages/About';
 
 const Main = () => {
   return (
-    <Container maxWidth="sm">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/result" component={Result} />
-        </Switch>
-      </BrowserRouter>
-    </Container>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/result" component={Result} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
 import SearchForm from '../components/top/SearchForm';
 
 const Home  = () => {
   return (
-    <>
+    <Container maxWidth="sm">
       <SearchForm />
-    </>
+    </Container>
   );
 }
 
