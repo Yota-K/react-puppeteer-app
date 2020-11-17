3257ae010d3c58b3c1dac14cb1570cf052207600
紅梅陽太 <powdersugar828828@gmail.com>
2020-11-17 22:20:42 +0900

    失敗時の文言とスタイル追加。

--- front/src/components/result/Failure.tsx
+++ front/src/components/result/Failure.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  resultTitle: {
+    fontSize: '28px',
+    marginBottom: '20px',
+    color: 'red',
+  },
+  h2: {
+    marginBottom: '10px',
+  },
+});
 
 const Failure: React.FC = () => {
+  const classes = useStyles();
+
   return (
     <>
-      <p>しっぱい</p>
+      <Typography variant="h2" align="center" className={classes.resultTitle}>
+        検索に失敗しました<br/>
+        URLが間違っていないか確認してください
+      </Typography>
     </>
   );
 };
